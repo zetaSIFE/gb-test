42177b4547205d260e028d8962f901d13d1f4da5
SeungbeomLee <seungbeom1103@zetalux.co.kr>
2023-05-10 16:53:00 +0900

    feat:산업투자효과분석서비스 layout 미완성

--- src/pages/industInvest/IndustInvest.jsx
+++ src/pages/industInvest/IndustInvest.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import SideBar from "./SideBar";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+`
 
 export default function IndustInvest() {
-  return <div>IndustInvest</div>;
+  return (
+    
+    <Container>
+    <SideBar />
+    </Container>
+    
+  )
 }
