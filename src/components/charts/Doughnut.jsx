3af5e3a24dd190cadc25a8e0eb7c1ec4dd5133ec
yhJung <slilver520@zetalux.co.kr>
2023-05-11 13:25:20 +0900

    차트 샘플 업로드드

---
+++ src/components/charts/Doughnut.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import ReactECharts from "echarts-for-react";
+import { graphic } from 'echarts'
+
+const Doughnut = () => {
+  const option = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: '5%',
+      left: 'center'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: true,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1048, name: '포항시',
+            itemStyle: {
+              // color: "rgba(63, 119, 15, 1)"
+              color: new graphic.LinearGradient(0, 0, 0, 1, [
+                { offset: 0, color: '#83bff6' },
+                { offset: 0.5, color: '#188df0' },
+                { offset: 1, color: '#188df0' }
+              ])
+            }
+          },
+          { value: 735, name: '경주시' },
+          { value: 580, name: '김천시' },
+          { value: 484, name: '안동시' },
+          { value: 300, name: '구미시' },
+        ]
+      }
+    ]
+  };
+  return (
+    <ReactECharts
+      option={option}
+      style={{ height: "100%" }}
+      // opts={{ renderer: "svg" }}
+    />
+  );
+};
+
+export { Doughnut };
