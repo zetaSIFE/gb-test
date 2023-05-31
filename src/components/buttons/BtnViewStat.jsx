6f15895b4ae9dc42a49dfce7be5b9a44957a9319
yhJung <slilver520@zetalux.co.kr>
2023-05-31 09:42:52 +0900

    style: 차트 타이틀 css수정

--- src/components/buttons/BtnViewStat.jsx
+++ src/components/buttons/BtnViewStat.jsx
@@ -8,12 +8,13 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   gap: 6px;
-  width: 125px;
+  width: 28px;
   height: 28px;
-  background: #e5e5e5;
+  background: #fcf2f2;
   border-radius: 24px;
   color: #333333;
   font-size: 14px;
+  /* border:1px solid #7EB3FF; */
   /* font-weight: 500; */
 `;
 
@@ -21,7 +22,7 @@ export const BtnViewStat = () => {
   return (
     <Container>
       <Statistics />
-      <div>통계표 보기</div>
+      {/* <div>통계표 보기</div> */}
     </Container>
   );
 };
